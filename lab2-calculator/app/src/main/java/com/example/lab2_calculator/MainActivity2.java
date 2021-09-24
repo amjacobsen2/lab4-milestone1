58e33c09650f73e80a105ad3182f0d494ba72ae5
Allison Jacobsen <amjacobsen2@wisc.edu>
2021-09-24 16:21:38 -0500

    Calculator

---
+++ lab2-calculator/app/src/main/java/com/example/lab2_calculator/MainActivity2.java
@@ -0,0 +1,25 @@
+package com.example.lab2_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        textView= (TextView) findViewById(R.id.textView5);
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("resultVal");
+        Log.i("result", str);
+        textView.setText(str);
+    }
+}
